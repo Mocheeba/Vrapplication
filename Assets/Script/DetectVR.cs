9e4579ee2bb12239a084a1bec96e02d300851c4e
Mocheeba <41023178+Mocheeba@users.noreply.github.com>
2022-09-29 18:32:00 +0200

    locomotion

---
+++ Assets/Script/DetectVR.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class DetectVR : MonoBehaviour
+{
+    public GameObject xrOrigin;
+    public GameObject desktopCharacter;
+    public bool startInVR = true;
+
+
+    void Start()
+    {
+        if (startInVR)
+        {
+            var xrSettings = XRGeneralSettings.Instance;
+            if (xrSettings == null)
+            {
+                Debug.Log("XRGeneralSettings is null");
+                return;
+            }
+
+            var xrManager = xrSettings.Manager;
+            if (xrManager == null)
+            {
+                Debug.Log("XRManagerSettings is null");
+                return;
+            }
+
+            var xrLoader = xrManager.activeLoader;
+            if (xrLoader == null)
+            {
+                Debug.Log("XRLoader is null");
+                xrOrigin.SetActive(false);
+                desktopCharacter.SetActive(true);
+                return;
+            }
+
+            Debug.Log("XRLoader is NOT null");
+            xrOrigin.SetActive(true);
+            desktopCharacter.SetActive(false);
+        }
+        else
+        {
+            xrOrigin.SetActive(false);
+            desktopCharacter.SetActive(true);
+        }
+    }
+}
